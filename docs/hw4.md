12e853936ba4d1a16d22ad20893a753ebe22a7e1
Kristoffer Allick <Kris.allick@gmail.com>
2024-09-11 11:36:10 -0600

    Finshed Hw4 and md file

---
+++ docs/hw4.md
@@ -0,0 +1,5 @@
+# Homework 4: Timed Counter
+## Overview
+For this homework we created a timed counter. When given a count time this Timed Counter will count up to the time divided by the FPGA clock period
+## Deliverables
+<Screenshot><img src="assets/Timed_counter_TB_AllCases_100ns_and_240ns.png">
---
+++ hdl/timed_counter/timed_counter.vhd
@@ -0,0 +1,41 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity timed_counter is
+
+generic (
+    clk_period : time;	--20ns
+    count_time : time
+);
+port (
+    clk : in std_ulogic;
+    enable : in boolean;
+    done : out boolean
+);
+end entity timed_counter;
+
+architecture timed_counter_arch of timed_counter is
+
+constant COUNTER_LIMIT : integer:= count_time/clk_period; --ex 40ns/20ns = 2
+signal clock_count: integer range 0 to 65535; --uses 16 bits instead of 32
+
+begin
+
+	counter: process(clk)
+	begin
+		if (rising_edge(clk)) then
+			if enable then
+				if (clock_count = COUNTER_LIMIT )then
+					done <= true;
+					clock_count<=0;
+				else
+					clock_count<=clock_count+1;
+					done<=false;
+				end if;
+			else
+				done<=false;
+			end if;
+		end if;
+	end process;
+
+end architecture;
